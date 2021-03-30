0e73e79107759eeaf84ce4cf179f55b00b2a9e1b
gondhes <fajarwirazdi@gmail.com>
2021-03-30 17:54:18 +0700

    custom hooks, rename component, add buttons

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,100 +1,72 @@
 import './App.css';
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import CountryList from './components/CountryList'
-
-// class App extends React.Component {
-//   constructor() {
-//     super()
-
-//     this.state = {
-//       title: 'The Nations',
-//       countries: [
-//         {
-//           alpha2Code: 'AF',
-//           name: 'Afghanistan',
-//           capital: 'Kabul',
-//           region: 'Asia',
-//           flag: ''
-//         },
-//         {
-//           alpha2Code: 'HK',
-//           name: 'Hong Kong',
-//           capital: 'City of Victoria',
-//           region: 'Asia',
-//           flag: ''
-//         },
-//         {
-//           alpha2Code: 'ID',
-//           name: 'Indonesia',
-//           capital: 'Jakarta',
-//           region: 'Asia',
-//           flag: ''
-//         }
-//       ]
-//     }
-//   }
-
-//   getCountries = () => {
-//     fetch('https://restcountries.eu/rest/v2/all')
-//     .then(res => res.json())
-//     .then(res => {
-//       console.log(res)
-//       this.setState({
-//         ...this.state,
-//         countries: res
-//       })
-//     })
-//     .catch(err => console.log(err))
-//   }
-
-//   componentDidMount() {
-//     this.getCountries()
-//   }
-
-//   render () {
-//     const {title, countries} = this.state
-
-//     return (
-//       <div>
-//         <h1 className="text-center">{title}</h1>
-//         <section className="product">
-//           <div className="container">
-//             <div className="row">
-//               {
-//                 countries.map((country, id) => {
-//                   return <CountryList country={country} id={id} key={country.alpha2Code}></CountryList>
-//                 })
-//               }
-//             </div>
-//           </div>
-//         </section>
-//       </div>
-//     )
-//   }
-// }
+import Country from './components/Country'
+import useFetch from './helpers/hooks/useFetch'
 
 function App() {
-  const [countries, setCountries] = useState([])
 
-  useEffect(() => {
-    fetch('https://restcountries.eu/rest/v2/all')
-    .then(res => res.json())
-    .then(countries => {
-      setCountries(countries)
-    })
-    .catch(err => console.log(err))
-  }, [])
+  let baseUrl = 'https://restcountries.eu/rest/v2'
+  let url = baseUrl+'/all'
+  const all = baseUrl+'/all'
+  const africa = baseUrl+'/region/africa'
+  const americas = baseUrl+'/region/americas'
+  const asia = baseUrl+'/region/asia'
+  const europe = baseUrl+'/region/europe'
+  const oceania = baseUrl+'/region/oceania'
+
+  function changeUrl(newUrl) {
+    if (newUrl === 'all') {
+      url = all
+    } else if (newUrl === 'africa') {
+      url = africa
+    } else if (newUrl === 'americas') {
+      url = americas
+    } else if (newUrl === 'asia') {
+      url = asia
+    } else if (newUrl === 'europe') {
+      url = europe
+    } else if (newUrl === 'oceania') {
+      url = oceania
+    }
+  }
+
+  const { data, loading, error } = useFetch(url)
+
+  if (loading) {
+    return (
+      <div className="text-center">
+        <img src='../public/ripple.svg' alt='loading..'></img>
+      </div>
+    )
+  }
+
+  if (error) {
+    return (
+      <div className="text-center">
+        <h2>Error: {error.message}</h2>
+      </div>
+    )
+  }
 
   return (
     <div>
-      {/* <h1 className="text-center">{title}</h1> */}
+      <h1 className="text-center">Nations Of The World</h1>
+      <div className="text-center">
+        <button onClick={() => changeUrl('all')}>All Countries</button>
+        <button onClick={() => changeUrl('africa')}>Africa</button>
+        <button onClick={() => changeUrl('americas')}>Americas</button>
+        <button onClick={() => changeUrl('asia')}>Asia</button>
+        <button onClick={() => changeUrl('europe')}>Europe</button>
+        <button onClick={() => changeUrl('oceania')}>Oceania</button>
+      </div>
+      
       <section className="product">
         <div className="container">
           <div className="row">
             {
-              countries.map(country => {
-                return <CountryList country={country} key={country.alpha2Code}></CountryList>
+              data.map(country => {
+                return <Country country={country} key={country.alpha2Code}></Country>
               })
             }
           </div>
---
+++ react-app/src/components/Country.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+
+function Country(props) {
+  const country = props.country
+  return (
+    <>
+    <div className="col-3 mt-5">
+      <Card className="bg-light mb-3 text-center" style={{ height: '20rem' }}>
+      <Card.Img className="div-img border mt-4" src={country.flag} alt="flag"></Card.Img>
+        <Card.Body>
+          <Card.Title>{country.alpha2Code}</Card.Title>
+          <Card.Text><b>{country.name}</b></Card.Text>
+          <Card.Text>{country.region}</Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+    </>
+  )
+}
+
+export default Country
