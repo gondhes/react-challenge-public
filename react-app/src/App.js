e12fb66252f66e58b555057ea6cb8fcaa369acb3
gondhes <fajarwirazdi@gmail.com>
2021-03-31 12:01:52 +0700

    routing home berhasil

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,66 +1,38 @@
 import './App.css';
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Country from './components/Country'
-import useFetch from './helpers/hooks/useFetch'
 import { Button } from 'react-bootstrap'
+import Home from './pages/Home'
+import Detail from './pages/Detail'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
 
 function App() {
 
-  const { data, loading, error, setNewUrl } = useFetch()
-
-  let url;
-  let baseUrl = 'https://restcountries.eu/rest/v2'
-
-  function changeUrl(newUrl) {
-    if (newUrl === 'all') {
-      url = `${baseUrl}/${newUrl}`
-    } else {
-      url = `${baseUrl}/region/${newUrl}`
-    } 
-    setNewUrl(url)
-  }
-
-  if (loading) {
-    return (
-      <div className="text-center">
-        <img src='../public/ripple.svg' alt='loading..'></img>
-      </div>
-    )
-  }
-
-  if (error) {
-    return (
-      <div className="text-center">
-        <h2>Error: {error.message}</h2>
-      </div>
-    )
-  }
-
   return (
-    <div>
-      <div className="text-center mt-5">
-      <h1>Countries Of The World</h1><br></br>
-        <Button variant="outline-info" onClick={() => changeUrl('all')}>All Countries</Button>{' '}
-        <Button variant="outline-info" onClick={() => changeUrl('africa')}>Africa</Button>{' '}
-        <Button variant="outline-info" onClick={() => changeUrl('americas')}>Americas</Button>{' '}
-        <Button variant="outline-info" onClick={() => changeUrl('asia')}>Asia</Button>{' '}
-        <Button variant="outline-info" onClick={() => changeUrl('europe')}>Europe</Button>{' '}
-        <Button variant="outline-info" onClick={() => changeUrl('oceania')}>Oceania</Button>
+    <Router>
+      <div>
+        <nav className="text-center mt-5">
+          <Link to="/">Home |</Link>
+          <Link to="/country/af"> Country</Link>
+        </nav>
+
+        <Switch>
+          <Route path="/country/:id">
+            <Detail />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
-      
-      <section className="product">
-        <div className="container">
-          <div className="row">
-            {
-              data.map(country => {
-                return <Country country={country} key={country.alpha2Code}></Country>
-              })
-            }
-          </div>
-        </div>
-      </section>
-    </div>
+    </Router>
+
   )
 }
 
---
+++ react-app/src/pages/Home.js
@@ -0,0 +1,67 @@
+import '../App.css';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Country from '../components/Country'
+import useFetch from '../helpers/hooks/useFetch'
+import { Button } from 'react-bootstrap'
+
+function Home() {
+
+  const { data, loading, error, setNewUrl } = useFetch()
+
+  let url;
+  let baseUrl = 'https://restcountries.eu/rest/v2'
+
+  function changeUrl(newUrl) {
+    if (newUrl === 'all') {
+      url = `${baseUrl}/${newUrl}`
+    } else {
+      url = `${baseUrl}/region/${newUrl}`
+    } 
+    setNewUrl(url)
+  }
+
+  if (loading) {
+    return (
+      <div className="text-center">
+        <img src='../public/ripple.svg' alt='loading..'></img>
+      </div>
+    )
+  }
+
+  if (error) {
+    return (
+      <div className="text-center">
+        <h2>Error: {error.message}</h2>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <div className="text-center mt-5">
+      <h1>Countries Of The World</h1><br></br>
+        <Button variant="outline-info" onClick={() => changeUrl('all')}>All Countries</Button>{' '}
+        <Button variant="outline-info" onClick={() => changeUrl('africa')}>Africa</Button>{' '}
+        <Button variant="outline-info" onClick={() => changeUrl('americas')}>Americas</Button>{' '}
+        <Button variant="outline-info" onClick={() => changeUrl('asia')}>Asia</Button>{' '}
+        <Button variant="outline-info" onClick={() => changeUrl('europe')}>Europe</Button>{' '}
+        <Button variant="outline-info" onClick={() => changeUrl('oceania')}>Oceania</Button>
+      </div>
+      
+      <section className="product">
+        <div className="container">
+          <div className="row">
+            {
+              data.map(country => {
+                return <Country country={country} key={country.alpha2Code}></Country>
+              })
+            }
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Home;
