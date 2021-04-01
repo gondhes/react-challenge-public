15a91f36510df2693837f7808350856367f52c40
gondhes <fajarwirazdi@gmail.com>
2021-04-01 17:50:40 +0700

    add to favorite berhasil tidak double

--- react-app/src/store/reducers/favoritesReducer.js
+++ react-app/src/store/reducers/favoritesReducer.js
@@ -6,7 +6,9 @@ function favoritesReducer(state = initialState, action) {
   const { type, payload } = action
   if(type === 'favorites/setFavorites') {
     let fav = state.data
-    fav.push(payload)
+    if(fav.find(country => country.alpha2Code === payload.alpha2Code) === undefined ) {
+      fav.push(payload)
+    }
     return { ...state, data: fav }
   }
   return state
